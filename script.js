e24bea4318c87d615bbc0fe2740ea9ff8b6ce382
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 15:36:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ fetch("kunta4500k_wgs84.geojson")
     .then(data => {
         L.geoJSON(data).addTo(map);
     });
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
