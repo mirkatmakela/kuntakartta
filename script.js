82757228c4198a1d6855b85ebb4eb4023f523797
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 21:21:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var map = L.map('map').setView([64.96, 27.59], 6);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+//L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //  maxZoom: 19,
+   // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//}).addTo(map);
 
 
 fetch("kunta4500k_wgs84.geojson")
