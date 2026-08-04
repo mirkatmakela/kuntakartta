39c0fa2028aa7b2518b4d9402b8b4098ed409e68
Mirka Mäkelä <mirka.t.makela@gmail.com>
2026-08-04 23:47:29 +0300

    Tassa versiossa kartan klikkaus muuttaa varia ja jaa muistiin

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-console.log("ohjelma kaynnistyi");
+//console.log("ohjelma kaynnistyi");
 
 const map = L.map('map');
+let kaydyt = JSON.parse(localStorage.getItem("kaydyt")) || {};
 
+function kuntavarit(feature) {
+    if (kaydyt[feature.properties.kunta]) {
+        return {
+            color: "black",
+            weight: 1,
+            fillColor : "green",
+        };
+    } else {
+        return {
+            color: "black",
+            weight: 1,
+            fillColor: "red",
+            fillOpacity: 0.5
+        };
+    }
+}
 
 fetch("kunta4500k_wgs84.geojson")
     .then(response => response.json())
     .then(data => {
        const kunnat =  L.geoJSON(data, {
-    style: function(feature) {
-        return {
-            color: "black",
-            weight: 1,
-            fillColor: "lightgreen",
-            fillOpacity: 0.5
-        };
-    }, 
+    style: kuntavarit, 
     onEachFeature: function(feature, layer) {
         const koodi = feature.properties.kunta;
         layer.bindPopup(feature.properties.nimi);
         layer.on("click", function() {
-            console.log("klikattiin");
+            
         if (!kaydyt[koodi]) {
         kaydyt[koodi] = {
             paiva: new Date().toLocaleDateString("fi-FI")}
+        layer.setStyle(kuntavarit(feature));
         }
-        layer.setStyle({
-        weight: 3,
-        color: '#666',
-        fillColor: "red"});
+        localStorage.setItem("kaydyt", JSON.stringify(kaydyt))
+
         });
     }
 }).addTo(map);
         map.fitBounds(kunnat.getBounds());
     })
 
-//tyhjä lista onko vierailtu
-const kaydyt = {};
-console.log("paastiin loppuun");
-console.log(kaydyt);
+
+//console.log("paastiin loppuun");
+
