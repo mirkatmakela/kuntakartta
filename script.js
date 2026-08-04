df643f4b451385ba3d3c2485db1c3488ac085a1f
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 11:41:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([64.96, 27.59], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
