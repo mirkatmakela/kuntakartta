1cefd11237464abb922c4eba5cf10446811f61f8
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 12:38:18 +0300

    Add marker to map
    Added a marker to the map at coordinates [65.0, 25.5].

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([65.0, 25.5]).addTo(map);
