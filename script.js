0d15d69aac95a5598b3fe5e2736f3b0cb5132aad
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 11:57:54 +0300

    Remove unnecessary lines from script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 alert("JavaScript toimii");
-
-var map = L.map('map').setView([64.96, 27.59], 7);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
