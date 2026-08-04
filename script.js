800bd43e09adb193be3340b63e192acf42b8ec29
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 16:04:00 +0300

    Change popup message in geoJSON feature layer
    Updated popup message for geoJSON layer interaction.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ fetch("kunta4500k_wgs84.geojson")
     }, 
     onEachFeature: function(feature, layer) {
         // tähän tulee klikkaustoiminto
-        layer.bindPopup(feature.properties.nimi);
+        layer.bindPopup("toimii");
     }
 }).addTo(map);
     });
