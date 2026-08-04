997c9c4b52c9cbb51d83c703f87e69d79247cec2
mirkatmakela <mirka.t.makela@gmail.com>
2026-08-04 22:19:06 +0300

    Change map zoom level from 3 to 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([64.96, 27.59], 3);
+var map = L.map('map').setView([64.96, 27.59], 6);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
